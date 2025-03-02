f7095293f105b13c9eb666bdf5b4eac20d858529
luisecarrillom <luisecarrillom@gmail.com>
2025-03-01 23:40:57 -0700

    Added declaring variables file

---
+++ aditional-practice/ch01-1-declaring-variables.js
@@ -0,0 +1,13 @@
+// To run this file, use the command: node ch01-1-declaring-variables.js
+
+var x = 32;
+x;
+var whereAmI = "Calgary, AB, Canada";
+console.log("x:", x);
+console.log("whereAmI:", whereAmI);
+
+var x = 35;
+x;
+var whereAmI = "Edmonton, AB, Canada";
+console.log("x:", x);
+console.log("whereAmI:", whereAmI);
