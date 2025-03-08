c7a5c5bc8aec15e5eedcff3f5f9cef1a173586f5
luisecarrillom <luisecarrillom@gmail.com>
2025-03-07 23:39:21 -0700

    Add variables practice file

---
+++ Ch02/02_06/ch02-06-variables.js
@@ -0,0 +1,20 @@
+// File: ch02-06-variables.js
+// Author: Luis Carrillo
+// Practice with variables in JavaScript
+
+// Declare constants (cannot change)
+const dozen = 12;  // One dozen = 12
+const halfDozen = 6;  // Half dozen = 6
+const bakersDozen = 13;  // Baker's dozen = 13
+
+// Error! Cannot change a constant
+// dozen = 13;  // ❌ This will not work
+
+// Declare a variable using "var"
+var cookieCount = 5;  // 5 cookies
+
+// Error! "let" cannot be declared twice in the same scope
+// let cookieCount = 5;  // ❌ This will not work
+
+// To run this file, use the command:
+// node ch02-06-variables.js
