1935d1bb44ccca10b861644a53f7b20a910fb665
luisecarrillom <luisecarrillom@gmail.com>
2025-03-10 23:19:05 -0600

    Add arrays practice file

---
+++ Ch03/03_05/ch03-arrays.js
@@ -0,0 +1,27 @@
+// File: ch03-arrays.js
+// Author: Luis Carrillo
+// Practice with arrays in JavaScript
+
+// Empty array
+var myArray = [];
+console.log("Empty array:", myArray);
+
+// Array with days
+var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday"];
+console.log("Days of the week:", daysOfTheWeek);
+
+// Mixed data array
+var myList = [0, 1, 2, "string1", "string2", "string3", true, false];
+console.log("My list:", myList);
+
+// Array of counties
+var counties = ["Belknap", "Strafford", "Carroll", "Rockingham"];
+console.log("Counties:", counties);
+
+// Complex array
+var listOfStuff = [{ name: "value" }, [1, 2, 3], true, "nifty"];
+console.log("List of stuff:", listOfStuff);
+console.log("Length of listOfStuff:", listOfStuff.length);
+
+// To run this file, use:
+// node ch03-arrays.js
